6b3eef811da9fd01a097e0624de366b4e21c8e7b
wuaisong <wuyaming1212@163.com>
2022-05-07 22:52:48 +0800

    分离单元测试和集成测试

--- api/src/ittest/java/wu/ai/song/api/ittest/ApiApplicationTestsIT.java
+++ api/src/ittest/java/wu/ai/song/api/ittest/ApiApplicationTestsIT.java
@@ -1,9 +1,10 @@
-package wu.ai.song.api.uttest;
+package wu.ai.song.api.ittest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @SpringBootTest
 //不启动服务器,使用mockMvc进行测试http请求。启动了完整的Spring应用程序上下文，但没有启动服务器
 @AutoConfigureMockMvc
+@ActiveProfiles("it")
 class ApiApplicationTestsIT {
 
     @Test
