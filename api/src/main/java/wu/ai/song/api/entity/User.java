ba513f25347667a4f533e42af375954ee407edd7
wuaisong <wuyaming1212@163.com>
2022-06-21 16:53:55 +0800

    主键配置失效问题复现

--- api/src/main/java/wu/ai/song/api/entity/User.java
+++ api/src/main/java/wu/ai/song/api/entity/User.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Setter
 @ToString
 public class User {
-    @TableId(type = IdType.AUTO)
+    @TableId(type = IdType.ASSIGN_ID)
     private Long id;
     private String name;
     private Integer age;
--- api/src/main/java/wu/ai/song/api/utils/MyBatisPlusConfig.java
+++ api/src/main/java/wu/ai/song/api/utils/MyBatisPlusConfig.java
@@ -1,14 +1,21 @@
 package wu.ai.song.api.utils;
 
-import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.util.CollectionUtils;
 
-import java.util.Properties;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @MapperScan("wu.ai.song.api.mapper")
 @Configuration
@@ -33,6 +40,32 @@ public class MyBatisPlusConfig {
     public PaginationInterceptor paginationInterceptor() {
         // 开启 count 的 join 优化,只针对部分 left join
         return new PaginationInterceptor();
+    }
+
+    @Bean
+    public MybatisSqlSessionFactoryBean mybatisSqlSessionFactoryBean(DataSource dataSource) {
+        MybatisSqlSessionFactoryBean sessionFactoryBean = new MybatisSqlSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setMapperLocations(resolveMapperLocations());
+        return sessionFactoryBean;
+    }
+
+    public Resource[] resolveMapperLocations() {
+        ResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
+        List<String> mapperLocations = new ArrayList<>();
+        mapperLocations.add("classpath:mapper/*Mapper*.xml");
+        List<Resource> resources = new ArrayList();
+        if (!CollectionUtils.isEmpty(mapperLocations)) {
+            for (String mapperLocation : mapperLocations) {
+                try {
+                    Resource[] mappers = resourceResolver.getResources(mapperLocation);
+                    resources.addAll(Arrays.asList(mappers));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return resources.toArray(new Resource[resources.size()]);
     }
 
 }
