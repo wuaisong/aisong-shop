463ba88515f2db0819b58e5c2c01eb0a0aa6176f
75318070 <yaming.wu@luxsan-ict.com>
2023-07-05 17:51:21 +0800

    引入router

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-main>
+      <el-header>
         <router-link to="/second">页面一</router-link>
         <router-link to="/first">页面二</router-link>
+      </el-header>
+      <el-main>
         <router-view></router-view>
       </el-main>
     </el-container>
