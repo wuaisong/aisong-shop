c867652373d6645fab8313210c95f6c7a1c4b399
75318070 <yaming.wu@luxsan-ict.com>
2023-07-05 18:04:47 +0800

    引入router

--- front/src/App.vue
+++ front/src/App.vue
@@ -11,30 +11,4 @@
     </el-container>
   </div>
 </template>
-<script lang="ts" setup>
-import {ref, watch} from 'vue'
-import {useRoute, useRouter} from "vue-router";
-import type {TabsPaneContext} from 'element-plus'
-
-const route = useRoute()
-const router = useRouter()
-
-const activeName = ref('first')
-watch(activeName, (newVal, oldVal) => {
-  console.log(`New: ${newVal}, Old: ${oldVal}`)
-  console.log(`${route.path}?tab=${newVal}`)
-  router.push(`${route.path}?tab=${newVal}`)
-})
-const handleClick = (tab: TabsPaneContext, event: Event) => {
-  console.log(tab, event)
-}
-</script>
-<style>
-.demo-tabs > .el-tabs__content {
-  padding: 32px;
-  color: #6b778c;
-  font-size: 32px;
-  font-weight: 600;
-}
-</style>
 
