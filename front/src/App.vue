7e5b59257ed59b26d050a9285c6e3009b221a6ce
75318070 <yaming.wu@luxsan-ict.com>
2023-07-05 18:37:10 +0800

    引入router

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,13 +2,28 @@
   <div class="common-layout">
     <el-container>
       <el-header>
-        <router-link to="/second">页面一</router-link>
-        <router-link to="/first">页面二</router-link>
+        <el-menu router :default-active="route.path" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+          <el-menu-item index="/first">Processing Center</el-menu-item>
+          <el-menu-item index="/second">Orders</el-menu-item>
+        </el-menu>
       </el-header>
       <el-main>
         <router-view></router-view>
       </el-main>
     </el-container>
   </div>
+
+
 </template>
 
+<script lang="ts" setup>
+import {ref} from 'vue'
+import {useRoute} from "vue-router"
+
+const route = useRoute();
+
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
