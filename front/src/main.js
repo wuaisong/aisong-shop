66dc06bba45d26e1a3a7aa2af963d0b2bf7024d5
wuaisong <wuyaming1212@163.com>
2023-07-23 07:49:52 +0800

    引入vxe-table

--- front/src/main.js
+++ front/src/main.js
@@ -4,6 +4,8 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import {createPinia} from 'pinia'
 import router from './router/index.js';
+import VXETable from 'vxe-table'
+import 'vxe-table/lib/style.css'
 
 const pinia = createPinia()
-createApp(App).use(ElementPlus).use(router).use(pinia).mount('#app');
+createApp(App).use(ElementPlus).use(VXETable).use(router).use(pinia).mount('#app');
