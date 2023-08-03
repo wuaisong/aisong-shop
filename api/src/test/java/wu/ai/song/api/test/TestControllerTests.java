bd1084bf5d97ee7b03415ab499f1643afafcaf70
wuaisong <wuyaming1212@163.com>
2023-08-03 09:15:04 +0800

    添加测试模块

--- api/src/test/java/wu/ai/song/api/test/TestControllerTests.java
+++ api/src/test/java/wu/ai/song/api/test/TestControllerTests.java
@@ -2,6 +2,7 @@ package wu.ai.song.api.test;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
---
+++ testing/src/main/java/net/javaguides/springboot/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot.service;
+
+import net.javaguides.springboot.model.Employee;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeService {
+    Employee saveEmployee(Employee employee);
+    List<Employee> getAllEmployees();
+    Optional<Employee> getEmployeeById(long id);
+    Employee updateEmployee(Employee updatedEmployee);
+    void deleteEmployee(long id);
+}
